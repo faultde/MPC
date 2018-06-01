8705aacd8efa41fc154b453ff2d0164c53a4af20
Zachary Coxe <34263547+faultde@users.noreply.github.com>
2018-06-01 18:06:39 -0500

    Added keydown functions, more sound, sample loop

--- JS/index.js
+++ JS/index.js
@@ -1,29 +1,122 @@
 let volume = .25;
 
+const idArr = $(".pad").map(function (index) {
+    return this.id;
+})
 
-let Howls = {};
-Howls.kick = new Howl({
+
+let loop = false;
+
+//Drum kit
+let kit = {};
+kit.kick = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Ac_K.wav'],
     volume: volume
 });
 
 
-Howls.snare = new Howl({
+kit.snare = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Aco_Snr.wav'],
     volume: volume
 });
 
-Howls.hihat = new Howl({
+kit.hihat = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Ac_H.wav'],
     volume: volume
 })
 
+kit.bongo1 = new Howl({
+    src: ['https://s3.amazonaws.com/cssmpc/Bngo_1.wav'],
+    volume: volume
+})
 
+kit.bongo2 = new Howl({
+    src: ['https://s3.amazonaws.com/cssmpc/Bngo_2.wav'],
+    volume: volume
+})
+
+kit.ohat = new Howl({
+    src: ['https://s3.amazonaws.com/cssmpc/CL_OHH2.wav'],
+    volume: volume
+})
+
+kit.what = new Howl({
+    src: ['https://s3.amazonaws.com/cssmpc/Chant+What+007+Lil+John.wav'],
+    volume: volume
+})
+
+kit.rise = new Howl({
+    src: ['https://s3.amazonaws.com/cssmpc/FX+Riser+001+Lex+Luger+Effect.wav'],
+    volume: volume
+})
+
+kit.reload = new Howl({
+    src: ['https://s3.amazonaws.com/cssmpc/War+FX+Gun+Reload+001.wav'],
+    volume: volume
+})
+
+
+//Music Sample
+
+let sample = {};
+sample.A = new Howl({
+    src: ['https://s3.amazonaws.com/cssmpc/CanadianBling.wav'],
+    volume: volume,
+    loop: true
+});
+
+
+
+//function for pad clicking
 $(".pad").click(function (e) {
     var target = e.target.id;
     $("#text-display").text(target);
 
-    if (Howls[target]) {
-        Howls[target].play();
+    if (kit[target]) {
+        kit[target].play();
     }
 })
+
+//Play Sample Loop
+$('#button3').click(function (e) {
+    $("#button3").toggleClass("samplePlay");
+
+    if (loop === false) {
+        loop = true;
+        console.log("loop begin")
+        sample.A.play();
+    } else {
+        console.log("loop end")
+        loop = false;
+        sample.A.stop();
+    }
+
+})
+
+//function for keydown
+
+function processId(id) {
+    console.log(id);
+}
+
+function handlekeydown(e) {
+    var element = $('[data-key=' + e.keyCode + ']');
+    if (element.length) {
+        console.log(element[0].id);
+        var idName = element[0].id;
+
+        //play sound
+        kit[idName].play();
+
+        //update screen
+        $("#text-display").text(idName);
+
+        //button press
+
+
+    }
+}
+
+
+
+window.addEventListener('keydown', handlekeydown)
