75bbab69285a4a68dea3cec30c64e716246b3321
Zachary Coxe <34263547+faultde@users.noreply.github.com>
2018-06-03 14:35:46 -0500

    Added button press animation

--- JS/index.js
+++ JS/index.js
@@ -1,62 +1,73 @@
 let volume = .25;
+let rate = 1.0;
+var knob = $('#knob');
+let loop = false;
 
 const idArr = $(".pad").map(function (index) {
     return this.id;
 })
 
 
-let loop = false;
 
-//Drum kit
+//Drum Kit Banks
+
 let kit = {};
 kit.kick = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Ac_K.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 });
-
 
 kit.snare = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Aco_Snr.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 });
 
 kit.hihat = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Ac_H.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 })
 
 kit.bongo1 = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Bngo_1.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 })
 
 kit.bongo2 = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Bngo_2.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 })
 
 kit.ohat = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/CL_OHH2.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 })
 
 kit.what = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/Chant+What+007+Lil+John.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 })
 
 kit.rise = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/FX+Riser+001+Lex+Luger+Effect.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 })
 
 kit.reload = new Howl({
     src: ['https://s3.amazonaws.com/cssmpc/War+FX+Gun+Reload+001.wav'],
-    volume: volume
+    volume: volume,
+    rate: rate
 })
 
 
-//Music Sample
+//Sample Bank
 
 let sample = {};
 sample.A = new Howl({
@@ -67,17 +78,37 @@ sample.A = new Howl({
 
 
 
-//function for pad clicking
+//Animation Functions
+
+
+
+
+
+//Mouse Clicks//
+
+//Pad Clicks
 $(".pad").click(function (e) {
     var target = e.target.id;
+    var pad = $(this);
+
+    //update screen
     $("#text-display").text(target);
+
+    //button press
+    pad.addClass("padActive");
+
+    setTimeout(function () {
+        pad.removeClass('padActive');
+    }, 100);
+
 
     if (kit[target]) {
         kit[target].play();
     }
 })
 
-//Play Sample Loop
+
+//Sample Clicks
 $('#button3').click(function (e) {
     $("#button3").toggleClass("samplePlay");
 
@@ -93,12 +124,10 @@ $('#button3').click(function (e) {
 
 })
 
-//function for keydown
 
-function processId(id) {
-    console.log(id);
-}
 
+
+//Keypress Events
 function handlekeydown(e) {
     var element = $('[data-key=' + e.keyCode + ']');
     if (element.length) {
@@ -110,8 +139,13 @@ function handlekeydown(e) {
 
         //update screen
         $("#text-display").text(idName);
+        element.addClass("padActive");
 
-        //button press
+        //button press animation
+        element.addClass("padActive");
+        setTimeout(function () {
+            element.removeClass('padActive');
+        }, 100);
 
 
     }
@@ -119,4 +153,4 @@ function handlekeydown(e) {
 
 
 
-window.addEventListener('keydown', handlekeydown)
+window.addEventListener('keydown', handlekeydown);
